d9043add46da16d51e36619b0887548f6b7db0ed
Tasha <tashingsworth@gmail.com>
2020-04-10 14:46:19 -0400

    figured out how to get stopPropagation to work, scaled boat image when mouse wheel is scrooled, changed color of nav container when mouse down, changed text color of nav heading when double clicked, changed header image when window resized. So far have 9 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,95 @@
 // Your code goes here
+
+
+
+// click event to change body background
+const container = document.querySelector('.home');
+
+container.addEventListener('click', () => {
+    container.style.backgroundColor = 'lightpink';
+});
+
+// add bus honk sound when mouse hovers over image 
+const play = document.querySelector('.intro img');
+play.addEventListener('mouseover', () => {
+    const audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'media/bus_honk.mp3');
+    audioElement.play();
+});
+
+// rotate images in content-section using gsap
+const adventureImg = document.querySelector('.img-content');
+
+adventureImg.addEventListener('click', (event) => {
+   event.stopPropagation();
+    gsap.to('.img-content', {
+        duration: 2,
+        filter: 'none',
+        x: -20,
+        y: -10,
+        rotate: -360,
+        yoyo: true,
+        repeat: 1
+    });
+});
+
+// scale text content
+const letsGo = document.querySelector('.text-content');
+
+letsGo.addEventListener('mouseenter', () => {
+    letsGo.style.transform = 'scale(1.5)';
+    letsGo.style.transition = 'transform 2s';
+});
+
+letsGo.addEventListener('mouseleave', () => {
+    letsGo.style.transform = 'scale(1)';
+});
+
+
+// scale boat image when wheel scrolls
+const boatImg = document.querySelector('.content-destination img');
+
+boatImg.addEventListener('wheel', () => {
+    boatImg.style.transform = 'scale(1.1)';
+    boatImg.style.transition = 'transform 1s';
+});
+
+// change nav container color when mouse pressed 
+const navColor = document.querySelector('.nav-container');
+
+navColor.addEventListener('mousedown', () => {
+    navColor.style.backgroundColor = 'seashell';
+});
+
+// change top 'logo' heading color when double clicked
+const logoHeadColor = document.querySelector('.logo-heading');
+
+logoHeadColor.addEventListener('dblclick', () => {
+    logoHeadColor.style.color = 'deepskyblue';
+});
+
+// chane header image when window resized
+const brokeVan = document.querySelector('.intro img');
+
+window.addEventListener('resize', () => {
+    brokeVan.src = 'img/broken_van.jpg';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
